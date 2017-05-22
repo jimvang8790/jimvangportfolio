040ad6f64fdeaea21d3c4570402076341f5ba6f5
Jim Vang <vangjim15@gmail.com>
2017-05-22 05:49:50 -0500

    added comments

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-// requires express, path, and variable app calling express
+// requires express and path in order to use them once installed
 var express = require('express');
 var path = require('path');
 var app = express();
 
-//
+//global variable, now port is not hard coded and app can be lanuch using heroku
+var port = process.env.PORT || 3004;
+
+// To serve static files such as images, CSS files, and JavaScript files, use the express.static built-in middleware function in Express.
 app.use(express.static('public'));
 
+// This should be the last route
+// /* is wildcard will respond to all requests
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/views/index.html'));
 });
 
 // spin up server and is listening on localhost:3004
-app.listen(3004, function() {
+app.listen(port, function() {
   console.log('up 3004');
 });
