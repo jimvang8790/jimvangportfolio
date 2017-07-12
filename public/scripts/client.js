1257dc456a9798e49294dbfca133ed1e0bc960c3
Jim Vang <vangjim15@gmail.com>
2017-07-12 15:19:24 -0500

    fixed grammer typo

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,7 +2,7 @@ var myApp = angular.module('myApp', ['ngRoute']);
 // Angular configuration (routes)
 myApp.config(function($routeProvider, $locationProvider) {
   $routeProvider.when('/', {
-    template: '<div class="home"><h1>Jim Vang</h1><h3>Prime Digital Academy | Full-Stack Software Engnieer</h3><h3>University of Minnesota | Chemistry</h3><img class="link" src="images/jimvang.jpg"></div>',
+    template: '<div class="home"><h1>Jim Vang</h1><h3>Prime Digital Academy | Full-Stack Software Engineer</h3><h3>University of Minnesota | Chemistry</h3><img class="link" src="images/jimvang.jpg"></div>',
     controller: ''
   }).when('/about', {
     templateUrl: 'views/about.html',
